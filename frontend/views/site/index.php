a8260b5e8d6258489c9de90759371cdd75fafae0
Чемерис Денис <denis.chemeris43@gmail.com>
2023-04-21 18:35:02 +0300

    Не работает кнопка Dropdown

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,8 +2,42 @@
 
 /** @var yii\web\View $this */
 
+use yii\bootstrap5\Dropdown;
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
+<!-- Эта кнопка не работает -->
+<?=Html::tag('div', Html::button('Action', ['class'=>'btn btn-danger dropdown-toggle', 'class-toggle' => 'dropdown', 'aria-haspopup' => 'true', 'aria-expanded' => 'false']).
+    Html::tag('div', Dropdown::widget(['items' => [
+        ['label' => 'Действие 1', 'url' => '#'],
+        ['label' => 'Действие 2', 'url' => '#'],
+        Html::tag('div', '', ['class' => 'dropdown-divider']),
+        ['label' => 'Действие 3']
+    ],])), ['class' => 'btn-group'])?>
+
+
+<!-- Этот код добавляется dropdown кнопку и добавляет скрипты и стили ('эта кнопка работает, но только с этими скриптами')-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<div class="btn-group">
+    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Action
+    </button>
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Действие 1</a>
+        <a class="dropdown-item" href="#">Дейсвтие 2</a>
+        <a class="dropdown-item" href="#">Дейсвтие 3</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Действие 4</a>
+    </div>
+</div>
+
+
+
+<!--
 <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
@@ -50,3 +84,4 @@ $this->title = 'My Yii Application';
 
     </div>
 </div>
+-->
